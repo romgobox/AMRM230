054bc40cf096d21c979dbc2aa21bdc3358f51e25
romgo <romgobox@gmail.com>
2015-06-15 12:39:00 +0500

    Реализован алгоритм чтения/записи команд, с количеством повторов и timeout ожидания ответа

--- dev_channel.py
+++ dev_channel.py
@@ -16,7 +16,8 @@ from CRC import CRC_M230
     
 class DirectChannel(object):
     
-    def __init__(self, port, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1, writeTimeout=1, ser = serial.Serial(), whTimeout = 0.1, whType=0):
+    def __init__(self, port, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1, writeTimeout=1, ser = serial.Serial(),\
+                 whTimeout = 0.1, attempt = 3, whType=0):
         """
         Открывает прямой канал (как правило по протоколу RS-485) до счетчика.
         whType:
@@ -32,6 +33,7 @@ class DirectChannel(object):
         self.ser.timeout = timeout
         self.ser.writeTimeout = writeTimeout
         self.whTimeout = whTimeout
+        self.attempt = attempt
         
         typeCRC = {
             0: CRC_M230(True),
@@ -47,44 +49,53 @@ class DirectChannel(object):
         else:
             logging.debug(u'Инициализация порта: ' + self.port)
     
-    def TXRX(self, cmd):
-        answer = []
-        ans=''
-        cmdTX = self.TX(cmd + self.CRC.calculate(cmd))
-        logging.debug(u'TX >>> ' + " ".join(cmdTX[0]) + ' [' + str(cmdTX[1]) +'] ')
+    def TXRX(self, cmd, byteExpected=0):
+        ansHex = []
+        ansChr=''
         
-        rx=3
+        self.attempt=3
         timeO=0
-        while rx>0:
+        
+        while self.attempt>0:
+            cmdTX = self.TX(cmd + self.CRC.calculate(cmd))
+            logging.debug(u'TX >>> ' + " ".join(cmdTX[0]) + ' [' + str(cmdTX[1]) +'] ')
+            
             while timeO < self.whTimeout:
                 time.sleep(0.1)
-                ans += self.RX()
-                answer += [chSim(hex(ord(x))[2:]) for x in ans]
-                if self.CRC.check(ans):
+                ansChr += self.RX()
+                ansHex += [chSim(hex(ord(x))[2:]) for x in ansChr]
+                
+                if self.CRC.check(ansChr):
                     timeO = self.whTimeout
-                    rx=0
-                    cmdRX = [answer, len(answer)]
+                    cmdRX = [ansHex, len(ansHex)]
                     logging.debug(u'RX <<< ' + " ".join(cmdRX[0]) + ' [' + str(cmdRX[1]) +'] ')
                 else:
-                    timeO+=0.1
-            if not self.CRC.check(ans):
-                rx=rx-1
-                timeO=0
-                #logging.debug(u'TX >>> ' + " ".join(self.TX(cmd + self.CRC.calculate(cmd))))
-                logging.debug(u'TX >>> ' + " ".join(cmdTX[0]) + '{' + str(cmdTX[1]) +'}')
-    
-        return answer   
+                    timeO += 0.1
+            if self.CRC.check(ansChr):
+                self.attempt = 0
+                answer = ansHex
+            else:
+                self.attempt -= 1
+                timeO = 0
+                answer = u'Нет ответа от устройства!'
+                logging.error(answer)
+        return answer
     
     def TX(self, cmd):
-        
+        """
+            Метод предназначен для отправки данных в порт
+            Принимает в качестве параметров команду
+            Возвращает список:
+            cmdTX[0] - список, отправленная команда в 16-м представлении
+            cmdTX[1] - количество отправленных байт
+        """
         self.ser.write(cmd)
         cmdsend = [chSim(hex(ord(x))[2:]) for x in cmd]
         cmdTX = [cmdsend, len(cmdsend)]
         return cmdTX
         
-    def RX(self):
-        
-       return self.ser.read(self.ser.inWaiting())
+    def RX(self):   
+        return self.ser.read(self.ser.inWaiting())
     
     
 
